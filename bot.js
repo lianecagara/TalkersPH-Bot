bcd83c65b5fcdc645497146798f8d48213d0501e
Liane Cagara (LiANE) <150021684+lianecagara@users.noreply.github.com>
2024-06-01 05:31:48 +0000

    What

--- bot.js
+++ bot.js
@@ -2,8 +2,18 @@ const Bot = require("./chatbot");
 const { LianeAPI } = require("fca-liane-utils");
 const bot = new Bot();
 const prefix = "/";
+const style = {
+  color: "white",
+  "font-weight": "bold",
+  "background-color": "blue",
+  padding: "1px 1px 1px",
+  "border-radius": "1px",
+};
+const mappedStyle = Object.entries(style)
+  .map(([key, value]) => `${key}: ${value}`)
+  .join(" ");
 
-bot.init("Jea", "http://localhost:8080").then(() => {
+bot.init(`Jea <span style="${mappedStyle}">BOT</span>`).then(() => {
   bot.sendMessage("Connected ✅");
 });
 
