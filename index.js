e8f842537b5f09e882e11e0cc7337744a29d55f0
Liane Cagara (LiANE) <150021684+lianecagara@users.noreply.github.com>
2024-06-01 02:57:45 +0000

    Update

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const Bot = require("./chatbot");
 const { LianeAPI } = require("fca-liane-utils");
 const bot = new Bot();
+const prefix = '/';
 
 bot.init("Jea");
 const jea = new LianeAPI("jea-mean", "lanceajiro");
@@ -9,6 +10,9 @@ bot.listen(async (event) => {
   /*if (event.body.startsWith('hi')) {
     bot.sendMessage(`Hello ${event.sender}!`);
   }*/
+  if (!event.body.startsWith(prefix + 'jea ')) {
+    return;
+  }
   const message = await jea.ask(`Sabi ni ${event.sender}: ${event.body}`);
   bot.sendMessage(`Replying to ${event.sender}:
 
