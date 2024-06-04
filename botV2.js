bc86c4512745b869823a3e12d53c3e5a3b587ff4
Liane Cagara (LiANE) <150021684+lianecagara@users.noreply.github.com>
2024-06-04 10:33:33 +0000

    fixes?

--- botV2.js
+++ botV2.js
@@ -1,5 +1,5 @@
 const { LLCBot } = require("./chatbotV2");
-const { LianeAPI } = require('fca-liane-utils');
+const { LianeAPI } = require("fca-liane-utils");
 
 const ai = new LianeAPI("bestgpt");
 
@@ -7,22 +7,35 @@ const bot = new LLCBot({
   username: "Test Bot",
 });
 bot.on("message", "message_reply", async (event) => {
+  function isCommand(key) {
+    return String(event.text)
+      .toLowerCase()
+      .startsWith("#" + String(key).toLowerCase());
+  }
   if (event.text === "hello") {
     const i = await bot.sendMessage(`Hello there ${event.username}!`, event);
-    console.log('Sent hi, ', i);
+    console.log("Sent hi, ", i);
   }
   if (event.text.includes("bot")) {
-    bot.animate(`Wow, hello ${event.username}! How can I assist you today?`, 50, event);
+    bot.animate(
+      `Wow, hello ${event.username}! How can I assist you today?`,
+      50,
+      event,
+    );
   }
-  if (event.text.toLowerCase().startsWith("#ai")) {
+  if (isCommand("ai")) {
     const question = event.text.slice(4).trim();
     const { raw } = await ai.request(question);
     bot.animate(raw, 50, event);
+  }
+  if (isCommand("uwu")) {
+    bot.animate("UwU, thank you!", 50, event);
   }
 });
 
 bot.on("ws_open", () => {
   bot.addCommand("#", "ai", "Ask AI anything!");
-})
+  bot.addCommand("#", "uwu", "Sends an uwu message!");
+});
 
 bot.startListening().catch(console.error);
